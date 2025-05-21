10f14048616bd51a3572e884c12285c67917c8c6
CODE\hafiz.mehdiyev <hafiz.mehdiyev@code.edu.az>
2025-05-21 17:02:37 +0400

    Some assets are changed to 512x512 HiearchyManager and ClearData Editor scripts are added.

---
+++ Assets/Editor/ClearData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ClearData : EditorWindow
+{
+    public static void ShowWindow()
+    {
+        GetWindow<ClearData>("ClearData");
+    }
+
+    [MenuItem("Local/Clear All Saved Data")]
+    public static void ClearAllData()
+    {
+        if (EditorUtility.DisplayDialog("Verileri Sil", "Tüm kayıtlı veriler silinsin mi?", "Evet", "Hayır"))
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+            Debug.Log("Tüm PlayerPrefs verisi silindi.");
+        }
+    }
+}
